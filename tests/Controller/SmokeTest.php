ebab5ff6fb3475076476035606e1f69084a38b49
louisbl <louis@beltramo.me>
2020-04-29 11:00:17 +0200

    test: smoke tests public urls

---
+++ tests/Controller/SmokeTest.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class SmokeTest extends WebTestCase
+{
+    /**
+     * @dataProvider generateUrls
+     */
+    public function testPublicPagesAreLoadingSuccessfully($url)
+    {
+        $client = static::createClient();
+
+        $client->request("GET", $url);
+
+        $this->assertResponseIsSuccessful("Home loaded successfully");
+    }
+
+    public function generateUrls()
+    {
+        return array(
+            array("/"),
+            array("/en/blog/"),
+        );
+    }
+
+    public function testBlogRedirectSuccessfully()
+    {
+        $client = static::createClient();
+
+        $client->request("GET", "/en/blog");
+
+        $this->assertResponseRedirects(
+            "http://localhost/en/blog/",
+            Response::HTTP_MOVED_PERMANENTLY,
+            "Blog redirects successfully"
+        );
+    }
+
+    public function testBlogPostLoadsSuccessfully()
+    {
+        $client = static::createClient();
+        // the service container is always available via the test client
+        $blogPost = $client->getContainer()->get('doctrine')->getRepository(Post::class)->find(1);
+
+        $client->request('GET', sprintf('/en/blog/posts/%s', $blogPost->getSlug()));
+
+        $this->assertResponseIsSuccessful();
+    }
+}

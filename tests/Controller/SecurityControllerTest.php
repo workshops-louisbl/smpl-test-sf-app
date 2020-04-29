a440d78970a3d119a09f97a8df13a5bd72ec35b1
louisbl <louis@beltramo.me>
2020-04-29 14:52:17 +0200

    test: login flow

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
 {
@@ -27,14 +28,67 @@ class SecurityControllerTest extends WebTestCase
         $this->assertSame($actualTitle, "Post List");
     }
 
-    public function testLoginWithUserISSuccessful()
+    public function testLoginWithUserAdminForbidden()
     {
-        // TODO: check redirect to app
+        $client = static::createClient();
+
+        $client->request("GET", "/en/login");
+
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $client->submitForm("Sign in", [
+            "_username" => "john_user",
+            "_password" => "kitten"
+        ]);
+
+        $this->assertResponseRedirects("http://localhost/en/admin/post/", Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    public function testLoginWithUserRedirectISSuccessful()
+    {
+        $client = static::createClient();
+
+        $client->request("GET", "/en/login?redirect_to=/en/blog/");
+
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $client->submitForm("Sign in", [
+            "_username" => "john_user",
+            "_password" => "kitten"
+        ]);
+
+        $this->assertResponseRedirects("http://localhost/en/blog/", Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertResponseIsSuccessful();
     }
 
     public function testLoginWithoutUserFail()
     {
-        // TODO: check login fail
+        $client = static::createClient();
+
+        $client->request("GET", "/en/login");
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm("Sign in", [
+            "_username" => "wrong user",
+            "_password" => "no password"
+        ]);
+
+        $this->assertResponseRedirects("http://localhost/en/login", Response::HTTP_FOUND);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertResponseIsSuccessful();
+
+        $actualMessage = $crawler->filter(".alert.alert-danger")->text();
+        $this->assertSame("Invalid credentials.", $actualMessage);
     }
 
 
